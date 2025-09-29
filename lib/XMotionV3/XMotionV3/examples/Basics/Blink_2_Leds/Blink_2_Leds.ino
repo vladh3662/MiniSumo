4cb5bbb4574076486e369ebc80ee0ab993b4d412
Vlad <vladh3662@gmail.com>
2025-09-29 20:33:34 +0300

    basic functions and programmed so that doesn't exit the ring

---
+++ lib/XMotionV3/XMotionV3/examples/Basics/Blink_2_Leds/Blink_2_Leds.ino
@@ -0,0 +1,9 @@
+#include <xmotionV3.h>
+
+void setup() {
+
+}
+
+void loop() {
+  xmotion.ToggleLeds(1000); // Toogle User Leds Blink with 1 sec. intervals.
+}
---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <Arduino.h>
+
+#define RightLine A2
+#define LeftLine A4
+
+const int LeftPWM = 10;
+const int LeftDir = 12;
+const int RightPWM = 11;
+const int RightDir = 13;
+
+void Forward(int speed){
+  digitalWrite(LeftDir, LOW);
+  digitalWrite(RightDir, LOW);
+  analogWrite(RightPWM, speed);
+  analogWrite(LeftPWM, speed);
+}
+
+void Backward(int speed){
+  digitalWrite(LeftDir, HIGH);
+  digitalWrite(RightDir, HIGH);
+  analogWrite(RightPWM, speed);
+  analogWrite(LeftPWM, speed);
+}
+
+void Left(int speed){
+  digitalWrite(LeftDir, LOW);
+  digitalWrite(RightDir, LOW);
+  analogWrite(RightPWM, speed);
+  analogWrite(LeftPWM, speed/4);
+}
+
+void Right(int speed){
+  digitalWrite(LeftDir, LOW);
+  digitalWrite(RightDir, LOW);
+  analogWrite(RightPWM, speed/4);
+  analogWrite(LeftPWM, speed);
+}
+
+void setup() {
+  pinMode(LeftDir, OUTPUT);
+  pinMode(LeftPWM, OUTPUT);
+  pinMode(RightDir, OUTPUT);
+  pinMode(RightPWM, OUTPUT);
+  pinMode(A0, INPUT);
+}
+
+void loop() {
+  Forward(255);
+  if(analogRead(RightLine)<100 || analogRead(LeftLine)<100){
+    Backward(150);
+    delay(350);
+    Left(150);
+    delay(500);
+  }
+}
+
